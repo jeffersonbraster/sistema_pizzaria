822dcca96fcd85725a8b3eb9390f7329ac0a57f9
Jefferson Brandao <jeffersonbraster@gmail.com>
2022-04-22 16:34:32 -0300

    iniciando estrutura web com next

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import styles from "../styles/Home.module.scss";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>JejePizza - Login</title>
+      </Head>
+      <div>
+        <h1>faça seu login</h1>
+      </div>
+    </>
+  );
+}
