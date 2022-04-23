0afe53e752ac6817a22d9e26d578c8fe89c3ee43
Jefferson Brandao <jeffersonbraster@gmail.com>
2022-04-22 21:18:33 -0300

    criando estilos pra login e cadastro

--- web/src/pages/signup/index.tsx
+++ web/src/pages/signup/index.tsx
@@ -1,33 +1,37 @@
 import Head from "next/head";
-import styles from "../styles/home.module.scss";
-import logoImg from "../../public/logo.svg";
+import styles from "../../styles/home.module.scss";
+import logoImg from "../../../public/logo.svg";
 import Image from "next/image";
-import { Input } from "../components/ui/Input";
-import Button from "../components/ui/Button";
+import { Input } from "../../components/ui/Input";
+import Button from "../../components/ui/Button";
 import Link from "next/link";
 
-export default function Home() {
+export default function Signup() {
   return (
     <>
       <Head>
-        <title>JejePizza - Login</title>
+        <title>JejePizza - Cadastro</title>
       </Head>
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt="Logo jeje-pizza" />
 
         <div className={styles.login}>
+          <h1>Criando sua conta</h1>
+
           <form>
+            <Input placeholder="Digite seu nome" type="text" />
+
             <Input placeholder="Digite seu E-mail" type="text" />
 
             <Input placeholder="Digite sua senha" type="password" />
 
             <Button type="submit" loading={true}>
-              Entrar
+              Cadastrar
             </Button>
           </form>
 
-          <Link href="/signup">
-            <a className={styles.text}>Não possui uma conta? Cadastre-se</a>
+          <Link href="/">
+            <a className={styles.text}>Já possui uma conta? Faça o login!</a>
           </Link>
         </div>
       </div>
