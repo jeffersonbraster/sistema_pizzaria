215194a872ace398cdd7975862bcfd58cfe19b72
Jefferson Brandao <jeffersonbraster@gmail.com>
2022-04-25 20:49:42 -0300

    criando nova categoria

--- web/src/pages/categories/index.tsx
+++ web/src/pages/categories/index.tsx
@@ -1,7 +1,9 @@
 import { NextPage } from "next";
 import Head from "next/head";
 import { FormEvent, useState } from "react";
+import { toast } from "react-toastify";
 import Header from "../../components/Header";
+import { setupApiClient } from "../../services/api";
 import styles from "./styles.module.scss";
 
 const Categories: NextPage = () => {
@@ -9,6 +11,19 @@ const Categories: NextPage = () => {
 
   const handleRegister = async (e: FormEvent) => {
     e.preventDefault();
+
+    if (name === "") {
+      return;
+    }
+
+    const apiClient = setupApiClient();
+
+    await apiClient.post("category", {
+      name,
+    });
+
+    toast.success("Categoria cadastrada com sucesso!");
+    setName("");
   };
 
   return (
